319091068def7a975ac6fb766eef614b25c9d255
erfan gandomkar <erfan.gandomkar2015@gmail.com>
2022-05-24 16:53:51 +0430

    working on constant movement
    using transform.position
    read about it before you use it ana

--- Assets/Scripts/charactercontrol.cs
+++ Assets/Scripts/charactercontrol.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using UnityEngine;
-using UnityEditor;
+﻿using UnityEngine;
 
 public class charactercontrol : MonoBehaviour
 {   
     public float movementSpeed = 50f;
     public Spawnmanager spawnmanager;
     public float speed = 5.0f;
+    public float Zspeed = 0.1f;
     
 
     // Start is called before the first frame update
@@ -18,14 +17,18 @@ public class charactercontrol : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Rigidbody.Velocity = Vector3(0, 0, 10);
+        transform.position += transform.position(0, 0, Zspeed) * Time.deltaTime;
+
         float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
         float vMovement = Input.GetAxis("Vertical") * movementSpeed;
 
         transform.Translate(new Vector3(0, 0, vMovement) * Time.deltaTime);
         transform.Rotate(0.0f, Input.GetAxis("Horizontal") * speed, 0.0f);
     }
-
+    void Update()
+    {
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
