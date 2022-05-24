5ab333a38e29f4d6668841f8682a31e9ab7f9898
erfan gandomkar <erfan.gandomkar2015@gmail.com>
2022-05-24 12:10:15 +0430

    start of the project
    this is just the begining

---
+++ Assets/Scripts/Roadspawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Roadspawner : MonoBehaviour
+{
+    public List<GameObject> RoadsFolder;
+    private float offset = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (RoadsFolder != null && RoadsFolder.Count > 0 )
+        {
+            RoadsFolder = RoadsFolder.OrderBy(r => r.transform.position.z).ToList();
+        }
+    }
+     
+    public void MoveRoad()
+    {
+        GameObject movedRoad = RoadsFolder[0];
+        RoadsFolder.Remove(movedRoad);
+        float newZ = RoadsFolder[RoadsFolder.Count - 1].transform.position.z + offset;
+        movedRoad.transform.position = new Vector3(0, 0, newZ);
+        RoadsFolder.Add(movedRoad);
+    }
+}
---
+++ Assets/Scripts/charactercontrol.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+public class charactercontrol : MonoBehaviour
+{   
+    public float movementSpeed = 50f;
+    public Spawnmanager spawnmanager;
+    public float speed = 5.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody.Velocity = Vector3(0, 0, 10);
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
+        float vMovement = Input.GetAxis("Vertical") * movementSpeed;
+
+        transform.Translate(new Vector3(0, 0, vMovement) * Time.deltaTime);
+        transform.Rotate(0.0f, Input.GetAxis("Horizontal") * speed, 0.0f);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        spawnmanager.SpawnTriggerEntered();
+    }
+}
