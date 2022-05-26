b4c991c43a2dc7037acea45c6ef5ff3875d0da2a
erfan gandomkar <erfan.gandomkar2015@gmail.com>
2022-05-26 10:18:05 +0430

    trying bullet creation and changing the speed physics
    https://docs.unity3d.com/ScriptReference/Object.Instantiate.html
    
    https://gamedevbeginner.com/how-to-move-objects-in-unity/#:~:text=from%20the%20keyboard.-,How%20to%20move%20an%20object%20with%20the%20keyboard%20in%20Unity,to%20use%20to%20control%20it.

--- Assets/Scripts/charactercontrol.cs
+++ Assets/Scripts/charactercontrol.cs
@@ -5,7 +5,9 @@ public class charactercontrol : MonoBehaviour
     public float movementSpeed = 50f;
     public Spawnmanager spawnmanager;
     public float speed = 5.0f;
+    public float Rspeed = 5.0f;
     public float Zspeed = 0.1f;
+    public Rigidbody rb;
     
 
     // Start is called before the first frame update
@@ -17,17 +19,39 @@ public class charactercontrol : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += transform.position(0, 0, Zspeed) * Time.deltaTime;
+        
+        //transform.position += new Vector3(0, 0, Zspeed) * Time.deltaTime;
+        if (speed < 20)
+        {
+            speed += 1f;
+        }
+        else if (speed > 20)
+        {
+            speed = 20f;
+        }
 
-        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 6;
         float vMovement = Input.GetAxis("Vertical") * movementSpeed;
 
         transform.Translate(new Vector3(0, 0, vMovement) * Time.deltaTime);
-        transform.Rotate(0.0f, Input.GetAxis("Horizontal") * speed, 0.0f);
+        transform.Rotate(0.0f, Input.GetAxis("Horizontal") * Rspeed, 0.0f);
     }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (speed > 0f)
+            {
+                speed -= 2f;
+            }
+        }
+
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        //output to log the position change
+        Debug.Log(transform.position);
+        Debug.Log(speed);
     }
 
     private void OnTriggerEnter(Collider other)
